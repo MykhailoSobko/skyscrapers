a11ca8e4a6e7c3c670ee02f1457e2b7a44a684b8
Mykhailo Sobko <m.t.sobko@gmail.com>
2021-02-06 02:28:20 +0200

    added LICENSE and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='skyscrapers',
+      version='1.0',
+      description='A logical python game',
+      author='Mykhailo Sobko',
+      author_email='m.t.sobko@gmail.com',
+      url='',
+      py_modules = ['skyscrapers']
+     )
